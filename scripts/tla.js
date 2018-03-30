f39a026d282b8222a4397b66b80b77edcce7fdd0
Ralf Brandenstein <nooitaf@gmail.com>
2018-03-30 11:45:33 +0200

    tla fix

--- scripts/tla.js
+++ scripts/tla.js
@@ -3,6 +3,12 @@ function tla(text){
   var txt = '' + text
   txt = txt.replace(/ /g, '')
   txt = txt.toUpperCase()
+  var over = txt.length % 3
+  if (over >= 1) {
+    for (var i=0;i<=3-over;i++){
+      txt = txt + ' '
+    }
+  }
   var txtArr = txt.match(/.{3}/g)
   txt = txtArr.join(' ')
   return txt
